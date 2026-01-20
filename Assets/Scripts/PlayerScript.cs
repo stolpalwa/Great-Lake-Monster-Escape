4c34ccf1b624d88721917431e0f27e7516fe1b19
annelie710221-max <annelie710221@gmail.com>
2026-01-20 23:51:45 +0100

    Update PlayerScript.cs

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -21,7 +21,7 @@ public class PlayerScript : MonoBehaviour
         playerRB.linearVelocity = new Vector3(0, (speed * Time.deltaTime) * Input.GetAxis("Vertical"), 0);
 
         //if (playerRB.linearVelocity.y > 0) // framåt rörelse
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             transform.position += new Vector3(0, 0, -1) * Time.deltaTime;
         }
