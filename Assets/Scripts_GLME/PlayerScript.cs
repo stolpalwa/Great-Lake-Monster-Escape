59385963c10241395b380f4039350e95698f2ed4
annelie710221-max <annelie710221@gmail.com>
2026-01-20 11:35:30 +0100

    fix kamera position

--- Assets/Scripts_GLME/PlayerScript.cs
+++ Assets/Scripts_GLME/PlayerScript.cs
@@ -18,12 +18,12 @@ public class PlayerScript : MonoBehaviour
         if (playerRB.linearVelocity.x < 0) // Vänster rörelse
         {
             // Moves the object forward at two units per second.
-            transform.position = new Vector3(-1, 0, 0) * Time.deltaTime;
+            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime;
             Debug.Log("Moving Left");
         }
         else if (playerRB.linearVelocity.x > 0) // Höger rörelse
         {
-            transform.position = new Vector3(1, 0, 0) * Time.deltaTime;
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
             Debug.Log("Moving Right");
         }
     }
