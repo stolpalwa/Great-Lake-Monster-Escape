58e04931496a3e893d88f4a8fa7642d0eb1121f2
annelie710221-max <annelie710221@gmail.com>
2026-01-21 20:22:10 +0100

    Update PlayerScript.cs

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour
 {
     public float fwdSpeed = 2f;     // Hastighet för båtens rörelse framåt
-    public AudioSource startSound;   // Nedräkningsljud vid start av spelet
+    public AudioSource startSound;  // Nedräkningsljud vid start av spelet
 
-    private bool canMove = false;
+    private bool canMove = false;   // Flagga för att kontrollera när båten kan börja röra sig
 
     void Start()
     {
@@ -22,7 +22,7 @@ public class PlayerScript : MonoBehaviour
         //}
 
         // TEST!!! Aktivera rörelse framåt när spelaren trycker ner spacetangenten
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (canMove && Input.GetKeyDown(KeyCode.Space))
         {
             transform.Translate(Vector3.forward * fwdSpeed * Time.deltaTime);
         }
