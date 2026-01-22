ac78e2ddfa74c5e24c272b9b0973cd98d972142d
annelie710221-max <annelie710221@gmail.com>
2026-01-22 11:01:31 +0100

    Update ObstackleScript.cs

--- Assets/Scripts/ObstackleScript.cs
+++ Assets/Scripts/ObstackleScript.cs
@@ -14,7 +14,7 @@ public class ObstackleScript : MonoBehaviour
             // samt spela upp ett ljud och en effekt vid kollision med ett hinder
             // OM Lager = "Obstacle" förflytta båten bakåt 1 position, (z = nuvarande position - 1)
             // OM Lager = "SpeedBoost" aktivera speed boost-funktionen i BoatMovementScript,
-            // OM Lager = "Kill" minska spelarens liv med 1, när liv = 0, game over,
+            // OM Lager = "Kill" minska spelarens liv med 1, när liv = 0, växla till game over scenen.
             
         }
     }
