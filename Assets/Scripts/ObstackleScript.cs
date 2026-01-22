68a7f198cf6ac99280668c1b53fdb43b2fbed6a7
annelie710221-max <annelie710221@gmail.com>
2026-01-22 11:00:36 +0100

    Fix lagt in kod i HinderScriptet

--- Assets/Scripts/ObstackleScript.cs
+++ Assets/Scripts/ObstackleScript.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class ObstackleScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
+        BoatMovementScript player = collision.gameObject.GetComponent<BoatMovementScript>();
+        if (player != null)
+        {
+            
+            Debug.Log("Båten har kolliderat med ett hinder!");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            // Vill ha kod för att kolla vilken typ av hinder båten kolliderar med (Layer),
+            // samt spela upp ett ljud och en effekt vid kollision med ett hinder
+            // OM Lager = "Obstacle" förflytta båten bakåt 1 position, (z = nuvarande position - 1)
+            // OM Lager = "SpeedBoost" aktivera speed boost-funktionen i BoatMovementScript,
+            // OM Lager = "Kill" minska spelarens liv med 1, när liv = 0, game over,
+            
+        }
     }
 }
